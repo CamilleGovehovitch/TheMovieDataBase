9d3b66fde1cf2fde3ac7c22da8fd548124cb7b47
camille <camille@cstar.io>
2024-10-18 11:51:04 +0200

    ADD movies list AND movie details

--- src/router/movieRoutes.ts
+++ src/router/movieRoutes.ts
@@ -1,10 +1,15 @@
 import { Router} from 'express';
-import { getMoviesDownload, getMovieByIdDownload} from '../controllers/movieController';
+import { getMoviesDownload, getMovieByIdDownload, getMovies, getMovieById} from '../controllers/movieController';
 
 const router: Router = Router();
 
 // GET all movies
+router.get('/movies', getMovies);
+// GET movie by id
+router.get('/movie/:id', getMovieById);
+// GET list of movies to download
 router.get('/movies-download', getMoviesDownload);
+// GET movie to download
 router.get('/movies-download/:id', getMovieByIdDownload);
 
 
